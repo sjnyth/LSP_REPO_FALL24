48307db83a4d0bdf5e2e0b795994635c2d1507cb
sjnth <sauja@10.0.0.11>
2024-09-12 13:13:25 -0400

    Add assignment 2

---
+++ LSP_Fall_2024/src/org/howard/lsp/assignment2/FileReader.java
@@ -0,0 +1,33 @@
+/**
+*  Name: Saujanya Thapaliya
+*/
+
+package org.howard.lsp.assignment2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class FileReader {
+
+	public String convertFileToString(String fileName) throws FileNotFoundException {
+		URL fileUrl = getClass().getClassLoader().getResource(fileName);
+		
+		if (fileUrl != null) {
+			File filePath = new File(fileUrl.getPath());
+			Scanner fileScanner = null;
+			try {
+				fileScanner = new Scanner(filePath);
+				String fileContent = "";
+				while (fileScanner.hasNextLine()) {
+					fileContent = fileContent + fileScanner.nextLine() + " ";
+				}
+				return fileContent.trim();
+			} finally {
+				fileScanner.close();
+			} 
+		}
+		throw new FileNotFoundException();
+	}
+}
---
+++ LSP_Fall_2024/src/org/howard/lsp/assignment2/WordCount.java
@@ -0,0 +1,46 @@
+/**
+*  Name: Saujanya Thapaliya
+*/
+
+package org.howard.lsp.assignment2;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		FileReader fileReader = new FileReader();
+		try {
+			String fileText = fileReader.convertFileToString("main/java/resources/words.txt");
+			calculateWordFrequency(fileText);
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		}
+	}
+	
+	static void calculateWordFrequency(String fileText) {
+		HashMap<String, Integer> wordCountMap = new HashMap<>();
+		
+		String cleanedText = fileText.replaceAll("[^a-zA-Z0-9\\s]", "");  
+        String[] wordArray = cleanedText.split("\\s+");
+        
+        for (String word : wordArray) {
+        	word = word.toLowerCase();
+        	boolean isNumber = word.chars().allMatch(Character::isDigit);
+        	if (!isNumber && word.length() > 3) {
+                if (wordCountMap.containsKey(word)) {
+                    wordCountMap.put(word, wordCountMap.get(word) + 1);
+                } else {
+                    wordCountMap.put(word, 1);
+                }
+        	}
+        }
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            String wordKey = entry.getKey();
+            int wordCount = entry.getValue();
+            System.out.println(wordKey + " " + wordCount);
+        }
+	}
+}
